29c9637db7da08380b096cd3baae8cb6dae6434a
tensilick <tensilick@proton.me>
2021-11-10 20:19:55 +0000

    More porting

--- build_kubeflow_pipeline.py
+++ build_kubeflow_pipeline.py
@@ -1,3 +1,12 @@
 # python build_kubeflow_pipeline.py <project_image>
 
-im
+import re
+from pathlib import Path
+from typing import Dict, Set
+
+import click
+
+from kfp import aws, dsl
+from kfp.compiler.compiler import Compiler
+
+from kedro.fram
