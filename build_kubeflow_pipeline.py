79dd3100e868f1f6806211d449b3846dc1c96e57
tensilick <tensilick@proton.me>
2021-11-23 06:13:35 +0000

    0.0.69

--- build_kubeflow_pipeline.py
+++ build_kubeflow_pipeline.py
@@ -11,4 +11,12 @@ from kfp.compiler.compiler import Compiler
 
 from kedro.framework.project import pipelines
 from kedro.framework.startup import bootstrap_project
-from kedro.pipeline.node import Nod
+from kedro.pipeline.node import Node
+
+_PIPELINE = None
+_IMAGE = None
+
+
+@click.command()
+@click.argument("image", required=True)
+@click.option("-p", "--pipeline", "pipeline_name", def
