22d8dca925eed49916ab93738a62734bc72afe53
tensilick <tensilick@proton.me>
2022-05-22 06:50:31 +0000

    Update yawsSocketConnect.js

--- src/kedro_tf_multimodal/pipeline_registry.py
+++ src/kedro_tf_multimodal/pipeline_registry.py
@@ -2,4 +2,9 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
-from kedro_tf_multimodal.pipelines.train.pipeline import create_pipelin
+from kedro_tf_multimodal.pipelines.train.pipeline import create_pipeline, create_train_pipeline
+
+# TODO: https://github.com/tensorflow/hub/issues/705
+import tensorflow_text as text
+
+def register_pipelines() -> D
