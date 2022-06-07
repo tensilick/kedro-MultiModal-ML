464b1ab5d863efb86505412d92d86504203966ec
tensilick <tensilick@proton.me>
2022-06-07 15:36:18 +0000

    Added a note

--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -7,4 +7,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from kedro_tf_utils.pipelines.train.pipeline import create_train_pipeline
 from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline
-from kedro.pipeline.modular_pipeline import pipeline as modular_pi
+from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
+from kedro_tf_text.pipelines.bert.pipeline import download_bert
+
+from kedro_tf_text.pipelines.tabular.pipeline import tabular_model_p
