86f1ae09dd7782c95314dce6563dd6ff4e8145f5
tensilick <tensilick@proton.me>
2022-06-07 14:09:55 +0000

    virtio-net: drop NETIF_F_FRAGLIST

--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -2,3 +2,7 @@
 This is a boilerplate pipeline 'train'
 generated using Kedro 0.18.1
 """
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from kedro_tf_utils.pipelines.train.pipeline import create_train_pipeline
