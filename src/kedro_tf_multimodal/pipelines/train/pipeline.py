f56596a3ef813b11028d1a00d70d10e571b9e8f0
tensilick <tensilick@proton.me>
2022-06-07 14:10:20 +0000

    Release 2.2.5

--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -6,3 +6,5 @@ generated using Kedro 0.18.1
 from kedro.pipeline import Pipeline, node, pipeline
 
 from kedro_tf_utils.pipelines.train.pipeline import create_train_pipeline
+from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline
+from kedro.pipeline.modular_pipeline import pipeline as modular_pi
