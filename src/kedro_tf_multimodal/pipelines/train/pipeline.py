5117daa13497938213806788c072badb16e1cf28
tensilick <tensilick@proton.me>
2022-06-08 11:04:01 +0000

    Regenerate gemspec for version 0.3.5

--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -14,4 +14,7 @@ from kedro_tf_text.pipelines.tabular.pipeline import tabular_model_pipeline
 from kedro_tf_image.pipelines.preprocess.pipeline import create_classification_layer
 
 from kedro_tf_text.pipelines.preprocess.pipeline import process_text_pipeline, glove_embedding
-from kedro_tf_text.pipelines.cnn.pipeline import cn
+from kedro_tf_text.pipelines.cnn.pipeline import cnn_text_pipeline
+
+# Download BERT model and save it in the TF Hub format in bert_model_saved catalog
+bert_model = modular_pipeline(pipe=download_b
