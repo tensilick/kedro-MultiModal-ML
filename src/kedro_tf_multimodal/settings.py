ef1e35e830afd07547856fc0fd9a7431d93efc58
tensilick <tensilick@proton.me>
2022-07-01 08:32:42 +0000

    grrr

--- src/kedro_tf_multimodal/settings.py
+++ src/kedro_tf_multimodal/settings.py
@@ -1,2 +1,5 @@
 """Project settings. There is no need to edit this file unless you want to change values
-from the Kedro defaults. For further information, includin
+from the Kedro defaults. For further information, including these default values, see
+https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
+
+# Instantiated project hooks.
