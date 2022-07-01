49092c3360b16b1523eb5e5bcb259ffdcf0fb608
tensilick <tensilick@proton.me>
2022-07-01 08:44:59 +0000

    removed node modules

--- src/kedro_tf_multimodal/settings.py
+++ src/kedro_tf_multimodal/settings.py
@@ -3,3 +3,7 @@ from the Kedro defaults. For further information, including these default values
 https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
+# from kedro_tf_multimodal.hooks import ProjectHooks
+# HOOKS = (ProjectHooks(),)
+
+# Installed plugins for which to disable hook auto-re
