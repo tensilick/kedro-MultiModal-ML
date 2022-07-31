6c653d069ef3f405ec00edae2c5a0690144f45ae
tensilick <tensilick@proton.me>
2022-07-31 17:59:04 +0000

    Update teledunetUrls.xml

--- src/kedro_tf_multimodal/settings.py
+++ src/kedro_tf_multimodal/settings.py
@@ -27,4 +27,11 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 # Class that manages how configuration is loaded.
 # from kedro.config import TemplatedConfigLoader
 # CONFIG_LOADER_CLASS = TemplatedConfigLoader
-# Keyword arguments to pass to the `CONFIG_LOADER_CLASS` construc
+# Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
+# CONFIG_LOADER_ARGS = {
+#     "globals_pattern": "*globals.yml",
+# }
+
+# Class that manages the Data Catalog.
+# from kedro.io import DataCatalog
+# DA
