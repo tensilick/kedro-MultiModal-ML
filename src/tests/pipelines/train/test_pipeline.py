1aefed5c7320ba718a442123dfd1defce9538325
tensilick <tensilick@proton.me>
2022-08-05 13:07:24 +0000

    Core: Fix warnings

--- src/tests/pipelines/train/test_pipeline.py
+++ src/tests/pipelines/train/test_pipeline.py
@@ -1,4 +1,8 @@
 """
 This is a boilerplate test file for pipeline 'train'
 generated using Kedro 0.18.1.
-Please add your pip
+Please add your pipeline tests here.
+
+Kedro recommends using `pytest` framework, more info about it can be found
+in the official documentation:
+https://
